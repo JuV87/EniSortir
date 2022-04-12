a5d57f673537c62ff76ce89811860e73aabe3768
JuV87 <99645371+JuV87@users.noreply.github.com>
2022-04-12 10:36:14 +0200

    trip controller
    trip controller

---
+++ src/Entity/Trip.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TripRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TripRepository::class)
+ */
+class Trip
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datestart;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateend;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbsubscriptionmax;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $descriptioninfos;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $statustrip;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $urlpicture;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $organizer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDatestart(): ?\DateTimeInterface
+    {
+        return $this->datestart;
+    }
+
+    public function setDatestart(\DateTimeInterface $datestart): self
+    {
+        $this->datestart = $datestart;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getDateend(): ?\DateTimeInterface
+    {
+        return $this->dateend;
+    }
+
+    public function setDateend(\DateTimeInterface $dateend): self
+    {
+        $this->dateend = $dateend;
+
+        return $this;
+    }
+
+    public function getNbsubscriptionmax(): ?int
+    {
+        return $this->nbsubscriptionmax;
+    }
+
+    public function setNbsubscriptionmax(int $nbsubscriptionmax): self
+    {
+        $this->nbsubscriptionmax = $nbsubscriptionmax;
+
+        return $this;
+    }
+
+    public function getDescriptioninfos(): ?string
+    {
+        return $this->descriptioninfos;
+    }
+
+    public function setDescriptioninfos(?string $descriptioninfos): self
+    {
+        $this->descriptioninfos = $descriptioninfos;
+
+        return $this;
+    }
+
+    public function getStatustrip(): ?int
+    {
+        return $this->statustrip;
+    }
+
+    public function setStatustrip(?int $statustrip): self
+    {
+        $this->statustrip = $statustrip;
+
+        return $this;
+    }
+
+    public function getUrlpicture(): ?string
+    {
+        return $this->urlpicture;
+    }
+
+    public function setUrlpicture(string $urlpicture): self
+    {
+        $this->urlpicture = $urlpicture;
+
+        return $this;
+    }
+}
